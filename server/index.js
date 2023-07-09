7f6fcfd2d50a3a178bef8cfb5f08fa37e2f5bea7
Shahel Tamrakar <rainfall037@gmail.com>
2023-07-09 15:19:16 +0545

    todos and subtasks

--- server/index.js
+++ server/index.js
@@ -9,3 +9,4 @@ app.use("/", indexRouter);
 app.listen(3000,()=>{
     console.log("Server running on port 3000");
 })
+
---
+++ server/modules/todos/todo.routes.api.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+
+router.get('/', (req, res)=>{
+    res.send("Todo API")
+})
+
+
+module.exports = router;
+
